e420e13736d3882092cf441aaa78039b0697dd4a
Masatomo Murata <masatomo.murata@justincase-tech.com>
2024-06-22 18:48:45 +0900

    feat: flyway自動生成ファイルの追加

--- flyway/build.gradle.kts
+++ flyway/build.gradle.kts
@@ -1,3 +1,6 @@
+import nu.studer.gradle.jooq.JooqEdition
+import org.jooq.meta.jaxb.Logging
+
 // この記載がないと No database found to handle になる
 buildscript {
     dependencies {
@@ -7,9 +10,11 @@ buildscript {
 
 plugins {
     id("org.flywaydb.flyway") version "10.11.0" // 10.14.0が最新だったが、 No database found to handleのエラーが出たため、出ない一番最新のバージョンを指定
+    id("nu.studer.jooq") version "9.0"
 }
 
 dependencies {
+    jooqGenerator("org.postgresql:postgresql:42.7.3")
     implementation("org.flywaydb:flyway-core:10.11.0")
     runtimeOnly("org.postgresql:postgresql:42.7.3")
 }
@@ -24,4 +29,41 @@ flyway {
 
 tasks.named("flywayMigrate") {
     dependsOn("classes")
+}
+
+jooq {
+    version.set("3.19.1")
+    edition.set(JooqEdition.OSS)
+
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/suppletrack"
+                    user = "postgres"
+                    password = "password"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+                    target.apply {
+                        packageName = "supplement"
+                        directory = "src/main/kotlin"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
 }
