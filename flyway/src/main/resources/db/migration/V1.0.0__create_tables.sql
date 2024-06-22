b744620c40f2aed8ae2e880dd80b98bc77c01537
Masatomo Murata <masatomo.murata@justincase-tech.com>
2024-06-22 18:47:53 +0900

    fix: テーブル定義の修正

--- flyway/src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ flyway/src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -1,7 +1,11 @@
 CREATE TABLE supplement_group
 (
-    name VARCHAR(255) PRIMARY KEY NOT NULL
+    name       VARCHAR(255) PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP                NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP                NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+COMMENT ON TABLE supplement_group IS 'サプリメントグループテーブル';
+COMMENT ON COLUMN supplement_group.name IS 'サプリメントグループ名';
 
 CREATE TABLE supplements
 (
@@ -12,6 +16,18 @@ CREATE TABLE supplements
     daily_intake_frequency INT          NOT NULL,
     expired_at             DATE         NOT NULL,
     start_at               DATE         NOT NULL,
-    end_dat                DATE         NOT NULL,
-    group_name VARCHAR(255) REFERENCES supplement_group (name)
+    end_at                 DATE         NOT NULL,
+    group_name             VARCHAR(255) REFERENCES supplement_group (name),
+    created_at             TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at             TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+COMMENT ON TABLE supplements IS 'サプリメントテーブル';
+COMMENT ON COLUMN supplements.id IS 'サプリメントID';
+COMMENT ON COLUMN supplements.name IS 'サプリメント名';
+COMMENT ON COLUMN supplements.quantity IS '総量';
+COMMENT ON COLUMN supplements.dosage_per_use IS '1回の摂取量';
+COMMENT ON COLUMN supplements.daily_intake_frequency IS '1日の摂取回数';
+COMMENT ON COLUMN supplements.expired_at IS '賞味期限';
+COMMENT ON COLUMN supplements.start_at IS '摂取開始日';
+COMMENT ON COLUMN supplements.end_at IS '摂取終了日';
+COMMENT ON COLUMN supplements.group_name IS 'サプリメントグループ名';
