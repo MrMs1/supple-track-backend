fcc9f176fb59c599aba373925ba9341fb3044947
Masatomo Murata <masatomo.murata@justincase-tech.com>
2024-06-24 22:57:01 +0900

    fix: Circular View Pathが発生するため、ControllerAdviceでResponseEntityを返却するように変更
    Stringを返却値にするとCircular View Pathになってしまうため、それを防止する

--- app/src/main/kotlin/com/mrms/SuppleTrack/presentation/advice/ExceptionHandler.kt
+++ app/src/main/kotlin/com/mrms/SuppleTrack/presentation/advice/ExceptionHandler.kt
@@ -2,21 +2,19 @@ package com.mrms.suppletrack.presentation.advice
 
 import com.mrms.suppletrack.domain.exception.DomainNotFoundException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
 
 @ControllerAdvice
 class ExceptionHandler {
     @ExceptionHandler(DomainNotFoundException::class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleDomainNotFoundException(e: DomainNotFoundException): String {
-        return "target not found."
+    fun handleDomainNotFoundException(e: DomainNotFoundException): ResponseEntity<String> {
+        return ResponseEntity("target not found.", HttpStatus.NOT_FOUND)
     }
 
     @ExceptionHandler(Exception::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun handleException(e: Exception): String {
-        return "internal server error."
+    fun handleException(e: Exception): ResponseEntity<String> {
+        return ResponseEntity("internal server error.", HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
