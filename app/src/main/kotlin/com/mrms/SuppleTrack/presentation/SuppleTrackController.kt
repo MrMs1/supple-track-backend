5bb3255d0a12c890bf5e5f50eebe0f1537024bef
Masatomo Murata <masatomo.murata@justincase-tech.com>
2024-06-30 00:09:34 +0900

    feat: Controllerではなく RestControllerを使用するように変更

--- app/src/main/kotlin/com/mrms/SuppleTrack/presentation/SuppleTrackController.kt
+++ app/src/main/kotlin/com/mrms/SuppleTrack/presentation/SuppleTrackController.kt
@@ -7,7 +7,6 @@ import com.mrms.suppletrack.presentation.dto.SupplementUpdateDto
 import com.mrms.suppletrack.usecase.SupplementUseCase
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -15,9 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
-@Controller
+@RestController
 @RequestMapping("/api")
 class SuppleTrackController
     @Autowired
